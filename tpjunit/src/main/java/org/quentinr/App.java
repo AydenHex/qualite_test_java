1c2abf2d25685d673c447db4a53d88a554f84e93
Singfield <rigaudlouis98@gmail.com>
2020-12-10 13:01:51 +0100

    add HomeView and beginning action Listener

--- tpjunit/src/main/java/org/quentinr/App.java
+++ tpjunit/src/main/java/org/quentinr/App.java
@@ -1,8 +1,29 @@
 package org.quentinr;
 
+import org.quentinr.gui.views.HomeViews;
+
 public class App {
     
     public static void main(String[] args) {
+        HomeViews test =new HomeViews("SquareRoot");
+        test.getCalculateSquare().addActionListener(new ActionListener(){
+            @Override
+            public voide actionPerformed (ActionEvent actionEvent){
+                    try {
+                        calculer();
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+            }
+        });
+    }
+
+    public static void calculer() {
+        int getValue1fromUser = Integer.parseInt(test.getValue1());
+        int getValue2fromUser = Integer.parseInt(test.getValue2());
+
+        Square square= new Square(getValue1fromUser, getValue2fromUser);
+
     }
     
 }
---
+++ tpjunit/src/main/java/org/quentinr/gui/views/HomeViews.java
@@ -0,0 +1,95 @@
+package org.quentinr.gui.views;
+
+import javax.swing.*;
+
+import java.awt.*;
+
+public class HomeViews extends JFrame{
+
+    //MainContent
+    private JPanel mainPan,btnPan,valuesPan;
+    
+    private JLabel resultLabel, value1Label, value2Label;
+    private JTextField value1, value2;
+    private JButton CalculateSquare;
+
+
+    public HomeViews(String titre) {
+        // Set the frame
+
+        setTitle(titre);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setPreferredSize(new Dimension(1000, 500));
+        this.setLayout(new BorderLayout());
+        setVisible(true);
+
+       
+        //Set the Panels
+        btnPan= new JPanel();
+        mainPan= new JPanel();
+        valuesPan=new JPanel();
+        valuesPan.setLayout(new FlowLayout());
+        btnPan.setBackground(Color.RED);
+        mainPan.setBackground(Color.ORANGE);
+
+
+        //Set Contents in Main Layout
+        
+        // Set JTextfield and Jlabel...
+        value1Label =new JLabel("Value 1 :");
+        value1=new JTextField(10);
+        value2Label =new JLabel("Value 2 :");
+        value2=new JTextField(10);
+        resultLabel=new JLabel("Test");
+        
+        // Set buttons
+        CalculateSquare =new JButton();
+        CalculateSquare.setText("Calculate");
+
+        // adding principals features
+        btnPan.add(CalculateSquare);
+        mainPan.add(resultLabel, BorderLayout.CENTER);
+            // adding valuesPan features
+        valuesPan.add(value1Label);
+        valuesPan.add(value1);
+        valuesPan.add(value2Label);
+        valuesPan.add(value2);
+
+        this.getContentPane().add(valuesPan, BorderLayout.NORTH);
+        this.getContentPane().add(mainPan, BorderLayout.CENTER);
+        this.getContentPane().add(btnPan, BorderLayout.SOUTH);
+        pack();
+
+    }
+
+
+    public JButton getCalculateSquare() {
+        return this.CalculateSquare;
+    }
+
+    public void setCalculateSquare(JButton CalculateSquare) {
+        this.CalculateSquare = CalculateSquare;
+    }
+
+
+    public JTextField getValue1() {
+        return this.value1;
+    }
+
+    public void setValue1(JTextField value1) {
+        this.value1 = value1;
+    }
+
+    public JTextField getValue2() {
+        return this.value2;
+    }
+
+    public void setValue2(JTextField value2) {
+        this.value2 = value2;
+    }
+
+
+}
+
